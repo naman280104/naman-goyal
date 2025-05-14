7772f92d0c71daa19643a2c0728e17c1f36ff682
Naman Goyal <naman280104@gmail.com>
2025-05-14 10:31:47 +0530

    adding lovable

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import { componentTagger } from "lovable-tagger";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   base: '/naman-goyal/',  // Replace with your repo name
   server: {
     host: "::",
@@ -11,10 +13,12 @@ export default defineConfig({
   },
   plugins: [
     react(),
-  ],
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+}));
