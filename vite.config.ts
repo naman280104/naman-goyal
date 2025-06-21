0588c8f1e6676a46196815aeddedc9d88ba30bd2
naman280104 <naman280104@gmail.com>
2025-06-21 20:09:36 +0530

    minor change

--- vite.config.ts
+++ vite.config.ts
@@ -21,13 +21,4 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  build: {
-    outDir: "dist",
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "index.html"),
-        fallback: path.resolve(__dirname, "index.html"),        
-      },
-    },
-  },
 }));
